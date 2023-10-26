401a29c1b31c63f0a0578f2f2ff6c74524ac1aaf
ItsMatDev <thierry.matthieu@live.fr>
2023-10-26 16:07:28 +0200

    readded deleted files

---
+++ frontend/src/components/LevelPage/LevelPage.jsx
@@ -0,0 +1,13 @@
+import GameMenu from "./GameMenu/GameMenu";
+import "./LevelPage.scss";
+
+function LevelPage() {
+  return (
+    <main>
+      <h1> Le Jurassique </h1>
+      <GameMenu />
+    </main>
+  );
+}
+
+export default LevelPage;
