423149eb869c67a0a00d3a5ba0e7e3427925275d
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-10-27 10:30:19 +0200

    modified paragraphs syntax and deleted br tags

--- frontend/src/components/HomePage/ContainerStoryRules/Rules/Rules.jsx
+++ frontend/src/components/HomePage/ContainerStoryRules/Rules/Rules.jsx
@@ -1,14 +1,15 @@
 function Rules() {
   return (
-    <p>
+    <section>
       <h2 className="rulesTitle">Règles du jeu</h2>
-      Cliquez sur "voyager", choisissez l’époque à visiter.
-      <br />
-      Dans l’interface du jeu, vous trouverez une liste d’artefacts à collecter.
-      Dans le niveau choisi, cliquez sur les éléments du décors pour en
-      apprendre plus sur l’histoire et retrouver le précieux artefacts tant
-      recherché.
-    </p>
+      <p>Cliquez sur "voyager", choisissez l’époque à visiter.</p>
+      <p>
+        Dans l’interface du jeu, vous trouverez une liste d’artefacts à
+        collecter. Dans le niveau choisi, cliquez sur les éléments du décorss
+        pour en apprendre plus sur l’histoire et retrouver le précieux artefacts
+        tant recherché.
+      </p>
+    </section>
   );
 }
 
