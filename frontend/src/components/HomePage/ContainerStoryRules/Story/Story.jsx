2edb2396b0b5824b00003c2b758e1b7dea3b529f
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-10-27 10:30:19 +0200

    modified paragraphs syntax and deleted br tags

--- frontend/src/components/HomePage/ContainerStoryRules/Story/Story.jsx
+++ frontend/src/components/HomePage/ContainerStoryRules/Story/Story.jsx
@@ -1,19 +1,22 @@
 function Story() {
   return (
-    <p>
+    <section>
       <h2 className="scenarioTitle">Scenario</h2>
-      Viktor, un jeune inventeur, passionné d’histoire, possède le
-      ChronoMécaniste, une machine à voyager dans le temps.
-      <br />
-      Afin d'enrichir sa collection d’objets anciens, il se lance dans cinq
-      époques majeures pour collecter des artefacts historiques
-      <br />
-      <br />
-      Depuis l'ère des dinosaures et l'égypte antique, en passant par la Rome
-      antique, le Moyen-Age et enfin l'ère moderne, aidez Viktor à voyager dans
-      le temps et à enrichir sa précieuse collection tout en apprenant de
-      précieuses leçons d'histoire.
-    </p>
+      <p>
+        Viktor, un jeune inventeur, passionné d’histoire, possède le
+        ChronoMécaniste, une machine à voyager dans le temps.
+      </p>
+      <p>
+        Afin d'enrichir sa collection d’objets anciens, il se lance dans cinq
+        époques majeures pour collecter des artefacts historiques
+      </p>
+      <p>
+        Depuis l'ère des dinosaures et l'égypte antique, en passant par la Rome
+        antique, le Moyen-Age et enfin l'ère moderne, aidez Viktor à voyager
+        dans le temps et à enrichir sa précieuse collection tout en apprenant de
+        précieuses leçons d'histoire.
+      </p>
+    </section>
   );
 }
 
