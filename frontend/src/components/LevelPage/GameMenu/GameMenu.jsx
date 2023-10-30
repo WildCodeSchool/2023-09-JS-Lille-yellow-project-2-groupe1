240a49d11b55ba76e84fb0a4024087b2c47c7f38
CYRIL DABURON <cyril.daburon@gmail.com>
2023-10-30 11:18:06 +0100

    new landscape in LevelPage / new pictures on the GameMenu and a new picture on the targetButton

--- frontend/src/components/LevelPage/GameMenu/GameMenu.jsx
+++ frontend/src/components/LevelPage/GameMenu/GameMenu.jsx
@@ -1,17 +1,25 @@
 import "./GameMenu.scss";
 import React from "react";
+import JurassiqueCharacter from "../../../assets/pictures/AvatarJurassique.png";
+import Bag from "../../../assets/pictures/Bag.png";
+import NoteBook from "../../../assets/pictures/NoteBook.png";
 
 function GameMenu() {
   return (
     <nav>
       <img
-        src="./frontend/src/assets/jurassique.png"
+        className="JurassiqueCharacter"
+        src={JurassiqueCharacter}
         alt="un homme de cromagnon"
       />
       <hr />
-      <button type="button">Objectifs</button>
+      <button type="button" className="GameMenuButton">
+        <img className="Bag" src={Bag} alt="a bagpack" />
+      </button>
       <hr />
-      <button type="button">Inventaire</button>
+      <button type="button" className="GameMenuButton">
+        <img className="NoteBook" src={NoteBook} alt="a notebook" />
+      </button>
     </nav>
   );
 }
