6203727247c93f7b613670402aa76c8e2b4e06d0
CYRIL DABURON <cyril.daburon@gmail.com>
2023-10-30 14:09:00 +0100

    fix a alt modification

--- frontend/src/components/LevelPage/GameMenu/GameMenu.jsx
+++ frontend/src/components/LevelPage/GameMenu/GameMenu.jsx
@@ -7,18 +7,14 @@ import NoteBook from "../../../assets/pictures/NoteBook.png";
 function GameMenu() {
   return (
     <nav>
-      <img
-        className="JurassiqueCharacter"
-        src={JurassicCharacter}
-        alt="Our main character"
-      />
+      <img className="JurassiqueCharacter" src={JurassicCharacter} alt="" />
       <hr />
       <button type="button" className="GameMenuButton">
-        <img className="Bag" src={Bag} alt="a bagpack" />
+        <img className="Bag" src={Bag} alt="Inventaire" />
       </button>
       <hr />
       <button type="button" className="GameMenuButton">
-        <img className="NoteBook" src={NoteBook} alt="a notebook" />
+        <img className="NoteBook" src={NoteBook} alt="Objectif" />
       </button>
     </nav>
   );
