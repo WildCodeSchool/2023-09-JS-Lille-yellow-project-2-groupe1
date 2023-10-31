60bcf5c32e417aff10ffb1aaf35c4db9ada6211e
Bravax <xavierlobez@hotmail.fr>
2023-10-31 11:25:36 +0100

    Correction of frenchglish

--- frontend/src/components/ChooseLevelPage/ChooseLevelPage.jsx
+++ frontend/src/components/ChooseLevelPage/ChooseLevelPage.jsx
@@ -2,16 +2,16 @@ import "./ChooseLevelPage.scss";
 
 function ChooseLevelPage() {
   const universe = [
-    { name: "CRÉTACÉ" },
-    { name: "ROMAIN" },
-    { name: "EGYPTE" },
-    { name: "MOYEN-AGE" },
-    { name: "MODERNE" },
+    { name: "CRÉTACÉ", Id: 1 },
+    { name: "ROMAIN", Id: 2 },
+    { name: "EGYPTE", Id: 3 },
+    { name: "MOYEN-AGE", Id: 4 },
+    { name: "MODERNE", Id: 5 },
   ];
   return (
     <main className="cardContainer">
       {universe.map((world) => (
-        <article key={world.name} className={`card card${world.name}`}>
+        <article key={world.name} className={`card card${world.Id}`}>
           <h2 className="titleEra">{world.name}</h2>
           <button type="button" className="cardContainer__playButton">
             Jouer
