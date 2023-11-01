332f446b0e71f8d6b8f57ee79a6541a535057f66
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-11-01 15:15:14 +0100

    added hover on chooseLevelPage/added new steampunk style button/ added level description / styled everything with transition

--- frontend/src/components/ChooseLevelPage/ChooseLevelPage.jsx
+++ frontend/src/components/ChooseLevelPage/ChooseLevelPage.jsx
@@ -1,4 +1,5 @@
 import "./ChooseLevelPage.scss";
+import button from "../../assets/pictures/playButtonSP.png";
 
 function ChooseLevelPage() {
   const universe = [
@@ -13,8 +14,18 @@ function ChooseLevelPage() {
       {universe.map((monde) => (
         <article key={monde.name} className={`card card${monde.name}`}>
           <h2 className="titleEra">{monde.name}</h2>
-          <button type="button" className="cardContainer__playButton">
-            Jouer
+          <p className="cardContainer__levelDescription">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            voluptates voluptatem porro voluptate similique rerum labore quos in
+            atque, omnis molestiae totam, eius beatae at asperiores cumque
+            soluta praesentium aliquid!
+          </p>
+          <button type="button">
+            <img
+              src={button}
+              alt="Bouton jouer"
+              className="cardContainer__playButton"
+            />
           </button>
         </article>
       ))}
