0a5e1c26df6c5c7a7dba5c72c8dfdb176db36f0c
Pierre-Adrien Gouillart <pagouillart@hotmail.fr>
2023-11-07 09:54:35 +0100

    added turnYourPhone scss in app.scss

--- frontend/src/components/ChooseLevelPage/ChooseLevelPage.jsx
+++ frontend/src/components/ChooseLevelPage/ChooseLevelPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "./ChooseLevelPage.scss";
 import button from "../../assets/pictures/playButtonSP.png";
+import TurnYourPhone from "../../assets/pictures/TurnYourPhone.jpg";
 
 function ChooseLevelPage() {
   const universe = [
@@ -11,33 +12,40 @@ function ChooseLevelPage() {
     { name: "MODERNE", id: 5 },
   ];
   return (
-    <main className="cardEraContainer">
-      {universe.map((world) => (
-        <article key={world.name} className={`card card${world.id}`}>
-          <h2 className="titleEra">{world.name}</h2>
-          <p className="cardEraContainer__levelDescription">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-            voluptates voluptatem porro voluptate similique rerum labore quos in
-            atque, omnis molestiae totam, eius beatae at asperiores cumque
-            soluta praesentium aliquid!
-          </p>
-          <Link to="/LevelPage">
-            <button type="button" className="playDevice">
-              <img
-                src={button}
-                alt="Bouton jouer"
-                className="cardEraContainer__playButton"
-              />
-            </button>
-          </Link>
-        </article>
-      ))}
-      <Link to="/">
-        <button type="button" className="closeButtonChooseLevelPage">
-          ↩
-        </button>
-      </Link>
-    </main>
+    <>
+      <img
+        className="turnYourPhone"
+        src={TurnYourPhone}
+        alt="Tournes ton téléphone en paysage"
+      />
+      <main className="cardEraContainer">
+        {universe.map((world) => (
+          <article key={world.name} className={`card card${world.id}`}>
+            <h2 className="titleEra">{world.name}</h2>
+            <p className="cardEraContainer__levelDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate voluptates voluptatem porro voluptate similique rerum
+              labore quos in atque, omnis molestiae totam, eius beatae at
+              asperiores cumque soluta praesentium aliquid!
+            </p>
+            <Link to="/LevelPage">
+              <button type="button" className="playDevice">
+                <img
+                  src={button}
+                  alt="Bouton jouer"
+                  className="cardEraContainer__playButton"
+                />
+              </button>
+            </Link>
+          </article>
+        ))}
+        <Link to="/">
+          <button type="button" className="closeButtonChooseLevelPage">
+            ↩
+          </button>
+        </Link>
+      </main>
+    </>
   );
 }
 
