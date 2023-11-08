cf053fc36188ba24d8ffd712e94cd4fa5a04d945
Romain <98345704+Romlion@users.noreply.github.com>
2023-11-08 12:10:28 +0100

    Popup Disign

---
+++ frontend/src/components/HomePage/ContainerStoryRules/TargetButton/TargetButton.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+import Pterodactyle from "../../../../assets/pictures/jurassic/Pterodactyle.png";
+import Brain from "../../../../assets/pictures/Brain.png";
+import Egg from "../../../../assets/pictures/jurassic/Pterodactyle-egg.png";
+
+import "./TargetButton.scss";
+
+function TargetButton() {
+  const [popupMessage, setPopupMessage] = useState("");
+  function HandleEvent() {
+    setPopupMessage(
+      <article className="popPtéro">
+        <img src={Pterodactyle} alt="Ptéro" className="Ptéro" />
+
+        <h1 className="name">Ptérodactyle</h1>
+        <img src={Brain} alt="cerveau" className="brain" />
+        <p className="info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem
+          voluptatibus magnam quo, maxime quod illo, reprehenderit quasi quas,
+          repudiandae amet aspernatur modi magni minus voluptatem rerum dolores
+          iste! Officia.
+        </p>
+        <button
+          type="button"
+          className="close"
+          onKeyDown={() => setPopupMessage("")}
+          onClick={() => setPopupMessage("")}
+        >
+          close
+        </button>
+        <button
+          type="button"
+          onKeyDown={HandleEvent}
+          onClick={HandleEvent}
+          className="buttonEgg"
+        >
+          <img src={Egg} alt="oeuf" className="Ptéro-egg" />
+        </button>
+      </article>
+    );
+  }
+  return (
+    <>
+      <button
+        type="button"
+        onKeyDown={HandleEvent}
+        onClick={HandleEvent}
+        className="buttonPtetodactyle"
+      >
+        <img src={Pterodactyle} alt="pterodactyle" className="Pterodactyle" />
+      </button>
+      {popupMessage}
+    </>
+  );
+}
+
+export default TargetButton;
