6c7398e8ac9f1c20a5e47960b3a534ab5995c9c9
ItsMatDev <thierry.matthieu@live.fr>
2023-11-09 09:57:51 +0100

    fixed display

--- frontend/src/components/LevelPage/LevelPage.jsx
+++ frontend/src/components/LevelPage/LevelPage.jsx
@@ -22,7 +22,7 @@ function LevelPage() {
   ) {
     return {
       data: dinosaursArray.find((dinosaure) => dinosaure.id === dinosaurId),
-      classtag,
+      class: classtag,
       item: { itemSrc, itemClass, itemClassButton },
       imgPopUpClass,
     };
