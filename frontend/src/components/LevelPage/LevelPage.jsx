f5884580a1310ae470126f68b349e2bfd8c2e923
ItsMatDev <thierry.matthieu@live.fr>
2023-11-15 11:49:28 +0100

    added dinosaurs and objet to velociraptor

--- frontend/src/components/LevelPage/LevelPage.jsx
+++ frontend/src/components/LevelPage/LevelPage.jsx
@@ -3,7 +3,6 @@ import { useGlobalContext } from "../Context/GlobalContextProvider";
 import GameMenu from "./GameMenu/GameMenu";
 import "./LevelPage.scss";
 import ReturnGameMenu from "./ReturnGameMenu/ReturnGameMenu";
-import JurassicTheme from "../../assets/pictures/jurassic/JurassicTheme.png";
 import TurnYourPhone from "../TurnYourPhone/TurnYourPhone";
 import TargetButton from "./TargetButton/TargetButton";
 import Quetzalcoatlus from "../../assets/pictures/jurassic/quetzalcoatlus.png";
@@ -11,6 +10,11 @@ import egg from "../../assets/pictures/jurassic/quetzalcoatlusEgg.png";
 import tyrannosaurusRex from "../../assets/pictures/jurassic/t-rex.png";
 import JurassicCharacter from "../../assets/pictures/jurassic/AvatarJurassic.png";
 import tooth from "../../assets/pictures/jurassic/tRexTooth.png";
+import Velociraptor from "../../assets/pictures/jurassic/velociraptor.png";
+import claw from "../../assets/pictures/jurassic/velociraptorclaw.png";
+import Triceratops from "../../assets/pictures/jurassic/triceraptops.png";
+import Brachiosaurus from "../../assets/pictures/jurassic/brachiosaurus.png";
+import Stegosaurus from "../../assets/pictures/jurassic/stegosaurus.png";
 
 function LevelPage() {
   const characterClass = "jurassicCharacter";
@@ -46,6 +50,32 @@ function LevelPage() {
   } else {
     tRex = getDinosaurData(1, "t-rex", "tRexPopUp");
   }
+
+  const triceratops = getDinosaurData(2, "triceratops", "triceratopsPopUp");
+
+  let velociraptor;
+  if (objectives.some((e) => e.item === "claw")) {
+    velociraptor = getDinosaurData(
+      3,
+      "velociraptor",
+      "velociraptorPopUp",
+      "claw",
+      claw,
+      "clawClass",
+      "clawClassButton"
+    );
+  } else {
+    velociraptor = getDinosaurData(3, "velociraptor", "velociraptorPopUp");
+  }
+
+  const brachiosaurus = getDinosaurData(
+    4,
+    "brachiosaurus",
+    "brachiosaurusPopUp"
+  );
+
+  const stegosaurus = getDinosaurData(5, "stegosaurus", "stegosaurusPopUp");
+
   let quetzalcoatlus;
   if (objectives.some((e) => e.item === "egg")) {
     quetzalcoatlus = getDinosaurData(
@@ -66,7 +96,7 @@ function LevelPage() {
   }
   return (
     <>
-      <main>
+      <main className="jurrassicTheme">
         <GameMenu
           bag={bag}
           characterImg={JurassicCharacter}
@@ -91,12 +121,38 @@ function LevelPage() {
           bag={bag}
           setBag={setBag}
         />
-
-        <img
-          className="jurassicTheme"
-          src={JurassicTheme}
-          alt="Des dinosaures dans un paysage du Crétacé"
-          draggable="false"
+        <TargetButton
+          data={velociraptor.data}
+          img={Velociraptor}
+          imgClass={velociraptor.imgClass}
+          item={velociraptor.item}
+          imgPopUpClass={velociraptor.imgPopUpClass}
+          bag={bag}
+          setBag={setBag}
+        />
+        <TargetButton
+          data={triceratops.data}
+          img={Triceratops}
+          imgClass={triceratops.imgClass}
+          imgPopUpClass={triceratops.imgPopUpClass}
+          bag={bag}
+          setBag={setBag}
+        />
+        <TargetButton
+          data={brachiosaurus.data}
+          img={Brachiosaurus}
+          imgClass={brachiosaurus.imgClass}
+          imgPopUpClass={brachiosaurus.imgPopUpClass}
+          bag={bag}
+          setBag={setBag}
+        />
+        <TargetButton
+          data={stegosaurus.data}
+          img={Stegosaurus}
+          imgClass={stegosaurus.imgClass}
+          imgPopUpClass={stegosaurus.imgPopUpClass}
+          bag={bag}
+          setBag={setBag}
         />
         <ReturnGameMenu />
       </main>
