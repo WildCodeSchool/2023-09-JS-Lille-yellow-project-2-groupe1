3dd508a7b6689e4b44bc0432fb67cf90f0e388b7
Bravax <xavierlobez@hotmail.fr>
2023-11-15 10:26:23 +0100

    Responsive Home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,6 @@ import LevelPage from "./components/LevelPage/LevelPage";
 import Egypt from "./components/LevelPage/Egypt";
 import Modern from "./components/LevelPage/ModernLevelPage/Modern";
 
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -35,12 +34,10 @@ const router = createBrowserRouter([
     loader: async () => {
       try {
         const responseEra = await fetch("http://localhost:3310/api/era");
-
         if (!responseEra.ok) {
           throw new Error("Failed to fetch era data");
         }
         const dataEra = await responseEra.json();
-
         return dataEra;
       } catch (error) {
         console.error(error);
@@ -55,12 +52,10 @@ const router = createBrowserRouter([
     loader: async () => {
       try {
         const responseDino = await fetch("http://localhost:3310/api/dinosaurs");
-
         if (!responseDino.ok) {
           throw new Error("Failed to fetch dinosaur data");
         }
         const dataDino = await responseDino.json();
-
         return dataDino;
       } catch (error) {
         console.error(error);
@@ -69,35 +64,35 @@ const router = createBrowserRouter([
     },
   },
   {
-
     path: "/Egypt",
     element: <Egypt />,
     id: "egypt",
     loader: async () => {
       try {
         const responseEgypt = await fetch("http://localhost:3310/api/egypt");
-
         if (!responseEgypt.ok) {
           throw new Error("Failed to fetch Egypt data");
         }
         const dataEgypt = await responseEgypt.json();
-
         return dataEgypt;
-
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    },
+  },
+  {
     path: "/Modern",
     element: <Modern />,
     id: "modern",
     loader: async () => {
       try {
         const responseModern = await fetch("http://localhost:3310/api/modern");
-
         if (!responseModern.ok) {
           throw new Error("Failed to fetch modern data");
         }
         const dataModern = await responseModern.json();
-
         return dataModern;
-
       } catch (error) {
         console.error(error);
         return null;
@@ -105,9 +100,7 @@ const router = createBrowserRouter([
     },
   },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <GlobalContextProvider>
